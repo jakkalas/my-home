c8ad1cca18e277faa198f3314d8bf3ef3d5202f4
Jaco Louw <jacolouw82@gmail.com>
2022-06-16 11:39:27 +0200

    :tada: just adding changes that wasn't saved

--- MyHomeApi/WebApi/Infrastructure/Smarthome/Providers/Ewelink/EwelinkService.cs
+++ MyHomeApi/WebApi/Infrastructure/Smarthome/Providers/Ewelink/EwelinkService.cs
@@ -1,6 +1,27 @@
-﻿namespace MyHomeApi.Infrastructure.Smarthome.Providers.Ewelink
+﻿using MyHomeApi.Infrastructure.Smarthome.Models;
+
+namespace MyHomeApi.Infrastructure.Smarthome.Providers.Ewelink
 {
-    public class EwelinkService
+    public class EwelinkService : ISmartHomeService
     {
+        public Device[] GetAllDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDeviceChannelCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetDevicePowerState(int? channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ToggleDevice(int? channel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
