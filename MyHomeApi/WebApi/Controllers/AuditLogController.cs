c86ab7dd139a60a1b8173e1e750ae1ef29e4c4c2
Jaco Louw <jaco.louw@codeo.co.za>
2022-09-30 16:12:54 +0200

    :construction: Added Auditlog service

---
+++ MyHomeApi/WebApi/Controllers/AuditLogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyHomeApi.Entities;
+using MyHomeApi.Services;
+
+namespace MyHomeApi.Controllers
+{
+	public class AuditLogController : Controller
+	{
+		private IAuditLogService _auditLogService;
+
+		public AuditLogController(IAuditLogService auditLogService)
+		{
+			_auditLogService = auditLogService;
+		}
+
+		[HttpGet]
+		public IActionResult GetAuditLog(int pageSize, int pageNumber)
+		{
+			var auditLogs = _auditLogService.GetAuditLogs(pageSize, pageNumber);
+			return Ok(auditLogs);
+		}
+	}
+}
